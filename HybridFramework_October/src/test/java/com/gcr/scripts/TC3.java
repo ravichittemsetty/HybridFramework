fc14b0f0a1a8b210e989c9ef30afc0731476a645
Ravi <ravichandrachittemsetty@gmail.com>
2022-12-08 07:09:11 -0800

    Added new Test case TC3

---
+++ HybridFramework_October/src/test/java/com/gcr/scripts/TC3.java
@@ -0,0 +1,20 @@
+package com.gcr.scripts;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.gcr.commands.ActionDriver;
+import com.gcr.config.StartBrowser;
+import com.gcr.reuse.CommonFunctions;
+
+public class TC3 extends StartBrowser{
+	@Test
+	public void onlyLoginLogout() throws Exception {
+		CommonFunctions cfs = new CommonFunctions();
+
+		cfs.login();
+
+		cfs.logout();
+	}
+		
+}
